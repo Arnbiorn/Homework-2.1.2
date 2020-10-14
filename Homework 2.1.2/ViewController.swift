2b62effc39ca7fe026c743162d09076722774127
Arnbiorn <skrutelev988@gmail.com>
2020-10-14 18:13:21 +0300

    Configured the display on different devices and switching traffic light colors

--- Homework 2.1.2/ViewController.swift
+++ Homework 2.1.2/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var turnOnButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        turnOnButton.layer.cornerRadius = 20
+        redLightView.layer.cornerRadius = redLightView.frame.size.width / 2
+        redLightView.alpha = 0.3
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.size.width / 2
+        yellowLightView.alpha = 0.3
+        greenLightView.layer.cornerRadius = greenLightView.frame.size.width / 2
+        greenLightView.alpha = 0.3
     }
-
-
+    
+    @IBAction func turnOnButtonPressed() {
+        guard redLightView.alpha == 1 || yellowLightView.alpha == 1 else {
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+            turnOnButton.setTitle("Next", for: .normal)
+            return
+        }
+        
+        guard yellowLightView.alpha == 1 else {
+            redLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            return
+        }
+        
+        guard greenLightView.alpha == 1 else {
+            yellowLightView.alpha = 0.3
+            redLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            return
+        }
+    }
 }
-
